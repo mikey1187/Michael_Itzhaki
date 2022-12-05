a669f64fac091dd9a87f0c78eac758355312fecf
mikey1187 <itzmich@post.bgu.ac.il>
2022-12-05 17:12:35 +0200

    Changing to 1 JS file

--- Part_B/Static/DoctorsJS.js
+++
@@ -1,3 +0,0 @@
-function toDoc(){
-    window.location.href = ("https://hmc.co.il/doctor/%D7%92%D7%99%D7%9C-%D7%A8%D7%A6%D7%91%D7%A1%D7%A7%D7%99/")
-}
---
+++ Part_B/Static/JS.js
@@ -0,0 +1,11 @@
+function ExpertsPage() {
+    window.location.href = ("file:///C:/Users/user/OneDrive%20-%20post.bgu.ac.il/Documents/%D7%94%D7%A9%D7%9B%D7%9C%D7%94/%D7%A1%D7%9E%D7%A1%D7%98%D7%A8%20%D7%94/WEB/Michael_Itzhaki/Part_B/View/ExpertPage.html");
+}
+
+function ToExperts(){
+    window.location.href = ("file:///C:/Users/user/OneDrive%20-%20post.bgu.ac.il/Documents/%D7%94%D7%A9%D7%9B%D7%9C%D7%94/%D7%A1%D7%9E%D7%A1%D7%98%D7%A8%20%D7%94/WEB/Michael_Itzhaki/Part_B/View/Doctors.html");
+
+}
+function toDoc(){
+    window.location.href = ("https://hmc.co.il/doctor/%D7%92%D7%99%D7%9C-%D7%A8%D7%A6%D7%91%D7%A1%D7%A7%D7%99/")
+}
