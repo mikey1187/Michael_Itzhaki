5b355aa50a0424c03c40c47f0f4fa1c78452c850
mikey1187 <itzmich@post.bgu.ac.il>
2022-12-05 16:42:07 +0200

    Skelton Steps

---
+++ Part_B/Static/HomePadeJS.js
@@ -0,0 +1,3 @@
+function ExpertPage() {
+    window.location.href = "file:///C:/Users/user/OneDrive%20-%20post.bgu.ac.il/Documents/%D7%94%D7%A9%D7%9B%D7%9C%D7%94/%D7%A1%D7%9E%D7%A1%D7%98%D7%A8%20%D7%94/WEB/Michael_Itzhaki/Part_B/View/ExpertPage.html";
+}
